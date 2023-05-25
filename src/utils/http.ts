c280b902ffbe4d25e9999545d17cd02630c7ebef
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-25 13:13:05 -0400

    base client

---
+++ src/utils/http.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+  headers: {
+    "Content-Type": `application/json`,
+  },
+});
+
+export default client;
