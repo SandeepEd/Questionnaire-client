9783c1ad93d08c0e584eae004567bbd11f46fd1d
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-25 13:12:43 -0400

    authentication service

---
+++ src/services/AuthService.ts
@@ -0,0 +1,21 @@
+import client from "../utils/http";
+import { IUser } from "../types/user";
+import cookie from "cookie";
+
+export class AuthService {
+  static login(data: IUser) {
+    return client.post("/login", data);
+  }
+
+  static signUp(data: IUser) {
+    return client.post("/sign-up", data);
+  }
+}
+
+export const login = async (data: IUser): Promise<IUser> =>
+  await client.post("/login", data);
+
+export const getSession = () => {
+  const { spa_token } = cookie.parse(document.cookie);
+  return spa_token;
+};
