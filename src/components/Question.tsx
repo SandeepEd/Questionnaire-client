3fccb9a8b4ada53dfe434b925dfa6cc05cfaef59
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-27 19:19:31 -0400

    use custom hook

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { IQuestion } from 'types/questionnaire';
+import { useQuestionData } from '../hooks/useQuestionData';
 
 interface Props {
   question: IQuestion;
+  button_text: string;
   callback: (e: React.FormEvent<EventTarget>, userAnswer: string) => void;
 }
 
-const Question: React.FC<Props> = ({ question, callback }) => {
-  const [ selectedOption, setSelectedOption ] = useState(``);
+const Question: React.FC<Props> = ({ question, button_text }) => {
 
-  const handleSelect = (option: string) => {
-    setSelectedOption(option);
-  };
-
-  const handleSubmit = (e: React.FormEvent<EventTarget>) => {
-    e.preventDefault();
-    callback(e, selectedOption);
-  };
-
+  const { handleSubmit, handleSelect, selectedOption } = useQuestionData(question);
   const options = question?.options;
 
   return (
@@ -27,11 +20,16 @@ const Question: React.FC<Props> = ({ question, callback }) => {
         {options?.map((option, index) =>
           <button
             key={index}
-            onClick={() => handleSelect(option.option_text)}
+            value={option.id}
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              handleSelect(option.id);
+            }}
             className={`min-w-full border-2 rounded border-solid border-gray-200 mb-1 
-            px-4 py-2 text-sm font-bold hover:bg-blue-300 hover:scale-105 
-            ${selectedOption === option.option_text ? `bg-green-400 text-white` :
-      `bg-white text-black`} focus:outline-none`}
+            px-4 py-2 text-sm font-bold hover:border-blue-700 hover:text-blue-700
+            ${selectedOption === option.id ? `bg-blue-800 text-white` :
+      `bg-white`} focus:outline-none`}
           >
             {option.option_text}
           </button>)}
@@ -39,7 +37,7 @@ const Question: React.FC<Props> = ({ question, callback }) => {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-2 py-2
           px-4 rounded focus:outline-none focus:shadow-outline float-right"
           type="submit"
-          value="Submit"
+          value={button_text}
         />
       </form>
     </div>
---
+++ src/hooks/useQuestionData.ts
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { IQuestion } from 'types/questionnaire';
+import { useQuiz } from '../context/QuizProviders';
+import { usePostAnswer } from '../services/AssignmentService';
+import { useNotification } from '../context/NotificationsProvider';
+
+export const useQuestionData = (question: IQuestion) => {
+
+  const { createNotification } = useNotification();
+  const { mutateAsync } = usePostAnswer();
+  const { goToNextQuestion } = useQuiz();
+  const [ selectedOption, setSelectedOption ] = useState<number | null>(question?.response_id);
+
+  const handleSelect = (option: number) => {
+    setSelectedOption(option);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<EventTarget>) => {
+    e.preventDefault();
+    if (!selectedOption) {
+      createNotification({
+        message: `Please select an option`,
+        type: `error`
+      });
+      return;
+    }
+    try {
+      await mutateAsync({
+        question_id: question?.id,
+        response_id: selectedOption
+      });
+      createNotification({
+        message: `Answer saved successfully`,
+        type: `success`
+      });
+      goToNextQuestion();
+    } catch (e: any) {
+      createNotification({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        message: e.response.data.message || e.message,
+        type: `error`
+      });
+    }
+  };
+
+  return { selectedOption, handleSelect, handleSubmit };
+};
