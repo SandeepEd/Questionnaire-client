fbc6b4ab0ac89f6ca1ab2f0cc33f29ad487a6e4b
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-27 19:20:19 -0400

    rename

---
+++ src/services/AssignmentService.ts
@@ -0,0 +1,31 @@
+import { AxiosResponse } from 'axios';
+import { QueryObserverResult, useMutation, useQuery, useQueryClient } from 'react-query';
+import { IQuestion } from '../types/questionnaire';
+import client from '../utils/http';
+
+export class QuestionService {
+  static getQuestions(): Promise<AxiosResponse<IQuestion[]>> {
+    return client.get(`/questionnaire/questions`);
+  }
+
+  static postResponse(question_id: number, response_id: number) {
+    return client.post(`/questionnaire/response`, { question_id, response_id });
+  }
+}
+
+export const useGetQuestions = (): QueryObserverResult<any> => useQuery({
+  queryKey: `questions`,
+  queryFn: () => QuestionService.getQuestions().then((res) => res.data),
+});
+
+export const usePostAnswer = () => {
+  const qc = useQueryClient();
+  return useMutation({
+    mutationFn: ({ question_id, response_id }: { question_id: number, response_id: number }) =>
+      QuestionService.postResponse(question_id, response_id),
+    onSuccess: async () => {
+      await qc.invalidateQueries(`questions`);
+    }
+  });
+
+};
