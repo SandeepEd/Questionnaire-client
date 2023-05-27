0725553c945fcc1d9ee6134832ebe0c44ca9b23e
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-27 00:15:47 -0400

    core functionality in quiz provider

--- src/context/QuizProviders.tsx
+++ src/context/QuizProviders.tsx
@@ -1,16 +1,33 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import Loading from '../components/Loading';
 import { useGetQuestions } from '../services/QuestionService';
 import { IQuestions } from '../types/questionnaire';
 
 interface IQuizContext {
-  questions: IQuestions | undefined;
+  questions: IQuestions[] | undefined;
+  currentQuestionIndex: number;
+  goToNextQuestion: () => void;
+  goToPreviousQuestion: () => void;
+  goToQuestion: (questionIndex: number) => void;
 }
 const QuizContext = createContext<IQuizContext | null>(null);
 
 export function QuizProvider({ children }: { children: React.ReactNode }) {
 
   const { isLoading, error, data } = useGetQuestions();
+  const [ currentQuestionIndex, setCurrentQuestionIndex ] = useState(0);
+
+  function goToNextQuestion() {
+    setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+  }
+
+  function goToPreviousQuestion() {
+    setCurrentQuestionIndex(prevIndex => prevIndex - 1);
+  }
+
+  function goToQuestion(questionIndex: number) {
+    setCurrentQuestionIndex(() => questionIndex);
+  }
 
   if (isLoading) {
     return <Loading />;
@@ -20,9 +37,13 @@ export function QuizProvider({ children }: { children: React.ReactNode }) {
     return <div>Something went wrong</div>;
   }
 
-  console.log(`data :::`, data);
-
-  return <QuizContext.Provider value={{ questions: data }}>
+  return <QuizContext.Provider value={{
+    questions: data,
+    currentQuestionIndex,
+    goToNextQuestion,
+    goToPreviousQuestion,
+    goToQuestion
+  }}>
     {children}
   </QuizContext.Provider>;
 }
