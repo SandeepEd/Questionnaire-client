cc43ea050f36edb8b42decec4b9cf1a71db01cac
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-27 22:07:48 -0400

    some styles changes for selected option after submission

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IQuestion } from 'types/questionnaire';
 import { useQuestionData } from '../hooks/useQuestionData';
+import { useAuth } from '../context/AuthContext';
 
 interface Props {
   question: IQuestion;
@@ -9,6 +10,8 @@ interface Props {
 }
 
 const Question: React.FC<Props> = ({ question, button_text }) => {
+
+  const { user } = useAuth();
 
   const { handleSubmit, handleSelect, selectedOption } = useQuestionData(question);
   const options = question?.options;
@@ -19,6 +22,13 @@ const Question: React.FC<Props> = ({ question, button_text }) => {
       <form onSubmit={handleSubmit}>
         {options?.map((option, index) =>
           <button
+            className={`min-w-full border-2 rounded border-solid border-gray-200 mb-1 
+            px-4 py-2 text-sm font-bold hover:border-blue-700 hover:text-blue-700
+
+            ${selectedOption === option.id ?
+      user?.assignment_submitted ? question.isCorrect ? `bg-green-500` : `bg-red-500 text-white`
+        : `bg-blue-800 text-white` : `bg-white`} focus:outline-none`}
+            disabled={user?.assignment_submitted}
             key={index}
             value={option.id}
             onClick={(e) => {
@@ -26,19 +36,16 @@ const Question: React.FC<Props> = ({ question, button_text }) => {
               e.stopPropagation();
               handleSelect(option.id);
             }}
-            className={`min-w-full border-2 rounded border-solid border-gray-200 mb-1 
-            px-4 py-2 text-sm font-bold hover:border-blue-700 hover:text-blue-700
-            ${selectedOption === option.id ? `bg-blue-800 text-white` :
-      `bg-white`} focus:outline-none`}
           >
             {option.option_text}
           </button>)}
-        <input
+        {!user?.assignment_submitted ? <input
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-2 py-2
-          px-4 rounded focus:outline-none focus:shadow-outline float-right"
+        px-4 rounded focus:outline-none focus:shadow-outline float-right"
+          disabled={user?.assignment_submitted}
           type="submit"
           value={button_text}
-        />
+        /> : null}
       </form>
     </div>
   );
