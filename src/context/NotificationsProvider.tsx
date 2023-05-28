fbd423ebf2e17efd77bbaac8c7176ff6f8ae4f23
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-28 16:50:01 -0400

    notification style changes

--- src/context/NotificationsProvider.tsx
+++ src/context/NotificationsProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, CSSProperties, useContext, useEffect, useState } from 'react';
 
 interface NotificationProps {
   title?: string;
@@ -24,6 +24,10 @@ export const NotificationsProvider: React.FC<{ children: JSX.Element }> = ({ chi
     setNotifications(() => []);
   };
 
+  const removeNotification = (index: number) => {
+    setNotifications((notifications) => notifications.filter((_, i) => i !== index));
+  };
+
   useEffect(() => {
     if (notifications.length > 0) {
       const timer = setTimeout(() => {
@@ -35,19 +39,23 @@ export const NotificationsProvider: React.FC<{ children: JSX.Element }> = ({ chi
 
   return <>
     <NotificationContext.Provider value={{ createNotification, clearNotifications }}>
-      {children}
       {notifications.map((notification, i) =>
         <div key={i} className={`fixed flex flex-row items-center justify-between 
-                    top-0 right-0 m-4 ${notification.type === `error` ? `bg-red-500 
-                    text-white` : `bg-green-300`}  px-3 py-2 rounded`}>
+        m-4 ${notification.type === `error` ? `bg-red-500 
+        text-white` : `bg-green-300`}  px-3 py-2 rounded`} style={{
+          top: `${i * 50}px`,
+          right: `0`,
+        } as CSSProperties}
+        >
           <div className="pr-4">
             {notification.message}
           </div>
-          {/* <button onClick={ }>
-                        X
-                    </button> */}
+          <button onClick={() => removeNotification(i)}>
+            X
+          </button>
         </div>)
       }
+      {children}
     </NotificationContext.Provider>
   </>;
 };
